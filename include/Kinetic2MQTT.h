de790c46309b95d8b2c087937c645e2d2454d75b
Glenn Pierce <glennpierce@gmail.com>
2025-06-21 22:00:29 +0100

    First Commit

---
+++ include/Kinetic2MQTT.h
@@ -0,0 +1,10 @@
+#ifndef KINETIC2MQTT_H
+#define KINETIC2MQTT_H
+
+// Function declarations
+void setFlag();
+void connectMqtt();
+//void bytesToHexString(uint8_t* byteArr, int length, char* output);
+//void publishMqtt(const char* topicLevel1, const char* topicLevel2, const char* messageValue);
+
+#endif // KINETIC2MQTT_H
